52649f59fb973633d61928c112a8eef46ca38d12
samweol <sejuyes@gmail.com>
2024-01-23 17:53:55 +0900

    SECTION1: 타스 컴파일러 옵션 설정하기

--- section1/dist/index.js
+++ section1/dist/index.js
@@ -1,5 +1,5 @@
 // import { hello } from "./hello";
 // hello("hi");
 // export {};
-
 const a = 1;
+export {};
---
+++ section1/src/hello.ts
@@ -0,0 +1,8 @@
+// export const hello = (message: string) => {
+//   console.log("hello" + message);
+// };
+
+// es-module로 작성한 파일
+// 매개변수의 타입은 TS가 추론할 수 없으므로 매개변수는 타입을 작성해줘야한다.
+
+const a = 1;
